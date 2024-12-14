77ef4c2f27d82f23666f0433181faf5ff04d70f0
Baiel Kanybekov <kanybekov245@gmail.com>
2024-12-14 21:00:08 +0600

    new commit

--- 06-section/tasks/03-task.ts
+++ 06-section/tasks/03-task.ts
@@ -2,7 +2,7 @@
 // ее дженерик таким образом чтобы код не вызывал
 // ошибок.
 
-function getLength<T>(arg: T): number {
+function getLength<T extends {length: number}>(arg: T): number {
     return arg.length;
   }
   
